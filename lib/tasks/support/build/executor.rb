b4f4d9a42b56f563d6b517e1c67882492f59cd97
chowlett <chowlett@oxbtech.com>
2025-05-29 13:26:13 -0400

    Merge main build code from the shell script version

--- lib/tasks/support/build/executor.rb
+++ lib/tasks/support/build/executor.rb
@@ -1,12 +1,36 @@
 require_relative '../app'
+require Rails.root.join('docker-build', 'version_tag').to_s
+require 'pathname'
+require 'open3'
 
 module Build
   class Executor
-    attr_accessor :app_name, :run_tests_please
+    VERSION_FILE_PATH = Rails.root.join('docker-build', 'version.json').to_s
+    ECR_REGISTRY = '938158173016.dkr.ecr.ca-central-1.amazonaws.com'
+
+    attr_accessor :appBrandName
+    attr_accessor :app_name
+    attr_accessor :branch
+    attr_accessor :version
 
     def execute
+      start_time = Time.now
       puts "Building #{app_name}"
       tests
+
+      ecr_login
+      change_to_context_dir
+      prepare_branch
+      prepare_version
+      puts "Building #{appBrandName} image #{branch_version}"
+
+      docker_build
+
+      save_version
+
+      elapsed_time = Time.now - start_time
+      puts "Completed building #{appBrandName} image #{branch_version}, elapsed #{'%.1f' % elapsed_time} secs."
+
     rescue StandardError => e
       puts "Error during build: #{e.message}"
       puts 'Build aborted.'
@@ -15,6 +39,7 @@ module Build
     def initialize(run_tests_please: true)
       self.app_name =App.app_name
       self.run_tests_please = run_tests_please
+      self.appBrandName = app_name == 'sitesource' ? 'SiTE SOURCE' : 'GRFS'
     end
 
     private
@@ -31,6 +56,74 @@ module Build
       raise "Test run failed with exit code #{exit_code}" unless exit_code.zero?
 
       puts "Tests passed"
+    end
+
+    def change_to_context_dir
+      Dir.chdir Rails.root
+    end
+
+    def prepare_branch
+      self.branch = `git branch --show-current`.strip
+      if branch.nil? || branch.length == 0
+        puts("Fatal. Unable to determine git branch.")
+        exit(8)
+      end
+    end
+
+    def prepare_version
+      self.version = VersionTag.new(VERSION_FILE_PATH)
+      version.inc
+    end
+
+    def docker_build
+      cmds = [
+        "docker buildx build -t #{ECR_REGISTRY}/#{app}:#{branch_version} --platform linux/arm64 --push ."
+      ]
+
+      cmds.each do |cmd|
+        puts cmd
+        output = `#{cmd}`
+        rc = $?.exitstatus
+
+        puts output
+        puts "rc = #{rc}"
+
+        if rc != 0
+          puts 'Command failed, terminating'
+          exit 8
+        end
+      end
+    end
+
+    def ecr_login
+      login_cmd = "aws ecr get-login-password | "\
+        "docker login --username AWS --password-stdin #{ECR_REGISTRY}"
+
+      Open3.popen3(login_cmd) do |stdin, stdout, stderr, thread|
+        process_status = thread.value
+        rc = process_status.exitstatus
+        stdout_lines = stdout.readlines.join('')
+        stderr_lines = stderr.readlines.join('')
+        if rc.zero?
+          puts stdout_lines
+        else
+          puts "login stdout: #{stdout_lines}" unless stdout_lines.strip == ''
+          puts "login stderr: #{stderr_lines}" unless stderr_lines.strip == ''
+          puts('Fatal error')
+          exit(8)
+        end
+      end
+    rescue StandardError => e
+      puts("Fatal docker login error: #{e.class} #{e}")
+      exit(8)
+    end
+
+    def save_version
+      version.save VERSION_FILE_PATH
+    end
+
+    def branch_version
+      "#{branch}-#{version}"
     end
   end
 end
